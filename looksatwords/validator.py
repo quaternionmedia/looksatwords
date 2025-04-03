6f8f7ac831ad03e529243a801946d6a41aceeb0d
Kevin Estavanik <kestavanik@gmail.com>
2025-04-03 18:49:15 -0400

    Added docstrings to validator.py

--- looksatwords/validator.py
+++ looksatwords/validator.py
@@ -2,6 +2,16 @@
 from pandera import Column, String, DataFrameSchema, Index, Object, Float, Int
 
 class BaseSchema:
+    """
+    Base class for defining schemas using pandera.
+
+    Methods:
+        generate_columns(prefix, suffixes, col_type):
+            Dynamically generates a dictionary of column definitions with a given prefix and type.
+
+        create_schema(columns_spec):
+            Constructs a Pandera DataFrameSchema from a dictionary of column definitions.
+    """
     @staticmethod
     def generate_columns(prefix, suffixes, col_type):
         return {f"{prefix}_{suffix}": Column(col_type, nullable=False) for suffix in suffixes}
@@ -16,9 +26,29 @@ wordcount_suffix = ['wordcount']
 pos_suffixes = ['noun', 'verb', 'adjective', 'adverb', 'pronoun', 'conjunction', 'preposition', 'interjection']
 
 class RawDataSchema(BaseSchema):
+    """
+    Schema definition for raw/unprocessed data. Currently empty, but acts as a placeholder for potential
+    validation rules for unstructured or minimally processed datasets.
+
+    Attributes:
+        schema (DataFrameSchema): An empty schema object.
+    """
     schema = BaseSchema.create_schema({})
 
 class GNewsDataSchema(BaseSchema):
+    """
+    Schema for validating the structure of raw data fetched from the GNews API..
+
+    Required Columns:
+        - headline (str): The article headline.
+        - description (str): The article description.
+        - url (str): URL to the full article.
+        - published date (str): Publication date in string format.
+        - publisher (object): Metadata about the publisher (can be nested).
+    
+    Attributes:
+        schema (DataFrameSchema): Schema object for GNews articles.
+    """
     schema = BaseSchema.create_schema({
         'headline': Column(String, nullable=False),
         'description': Column(String, nullable=False),
@@ -28,6 +58,13 @@ class GNewsDataSchema(BaseSchema):
     })
 
 class AnalyzedDataSchema(BaseSchema):
+    """
+    Schema for validating the additional analyzed data columns produced in analyzer.py 
+    after processing GNews API data.
+
+    Attributes:
+        schema (DataFrameSchema): Schema object for validated, analyzed news articles.
+    """
     columns_spec = {
         'headline': Column(String, nullable=False),
         'description': Column(String, nullable=False),
